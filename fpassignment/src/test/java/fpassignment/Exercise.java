fccfdff370f2b37824a3995bf245f050ada15cb3
vinilcodes <vinilkumar653@gmail.com>
2024-11-27 18:44:06 +0530

    Update Exercise.java

--- fpassignment/src/test/java/fpassignment/Exercise.java
+++ fpassignment/src/test/java/fpassignment/Exercise.java
@@ -14,33 +14,34 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Exercise {
 	@Test
-	public void test() {
+	public void newmethod() throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		
+
 		// Navigate to the FitPeo Homepage
 		String HomePage = "https://www.fitpeo.com/";
 		driver.get(HomePage);
-		
-		//Navigate to the Revenue Calculator Page
+
+		// Navigate to the Revenue Calculator Page
 		driver.get("https://fitpeo.com/revenue-calculator");
-		
-		//Scroll Down to the Slider section
+
+		// Scroll Down to the Slider section
 		JavascriptExecutor jse = (JavascriptExecutor) driver;
 		jse.executeScript("window.scrollBy(0,500)");
-		
-		//Adjust the Slider
+
+		// Adjust the Slider
 		WebElement Slider = driver.findElement(By.xpath(
 				"//span[@class='MuiSlider-thumb MuiSlider-thumbSizeMedium MuiSlider-thumbColorPrimary MuiSlider-thumb MuiSlider-thumbSizeMedium MuiSlider-thumbColorPrimary css-1sfugkh']"));
 		Actions action = new Actions(driver);
 		action.dragAndDropBy(Slider, 94, 0).perform();
-		
-		//Update the Text Field
+
+		// Update the Text Field
 		action.click(driver.findElement(By.xpath("//input[@id=':R57alklff9da:']"))).keyDown(Keys.CONTROL).sendKeys("a")
 				.keyUp(Keys.CONTROL).sendKeys(Keys.BACK_SPACE).sendKeys("560").build().perform();
+		Thread.sleep(2000);
 
-		//Select CPT Codes
+		// Select CPT Codes
 		jse.executeScript("window.scrollBy(0,250)");
 		driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div[1]/label/span[1]/input")).click();
 		driver.findElement(By.xpath("//div[@class='MuiBox-root css-1p19z09']//div[2]//label[1]//span[1]//input[1]"))
@@ -48,12 +49,14 @@ public class Exercise {
 		driver.findElement(By.xpath("//div[3]//label[1]//span[1]//input[1]")).click();
 		jse.executeScript("window.scrollBy(0,250)");
 		driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div[8]/label/span[1]/input")).click();
+		Thread.sleep(1000);
 
-		//Validate Total Recurring Reimbursement
+		// Validate Total Recurring Reimbursement
 		jse.executeScript("window.scrollBy(0,-900)");
 		action.click(driver.findElement(By.xpath("//input[@id=':R57alklff9da:']"))).keyDown(Keys.CONTROL).sendKeys("a")
 				.keyUp(Keys.CONTROL).sendKeys(Keys.BACK_SPACE).sendKeys("820").build().perform();
 		jse.executeScript("window.scrollBy(0,700)");
+		Thread.sleep(3000);
 		driver.quit();
 	}
 }
